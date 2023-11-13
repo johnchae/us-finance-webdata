cd05af1f3111d4f36fc9e091d57abda8dc6a5710
John Chae <johnchae1994@yahoo.com>
2023-11-13 03:17:04 -0500

    Update script.py

--- script.py
+++ script.py
@@ -4,19 +4,20 @@ import pandas_datareader.wb as wb
 from datetime import datetime
 import numpy as np
 
+def log_return(prices):
+  return np.log(prices / prices.shift(1))
+
 gold_prices = pd.read_csv('gold_prices.csv')
 crudeoil_prices = pd.read_csv('crude_oil_prices.csv')
+
+nasdaq_data = web.DataReader('NASDAQ100', 'fred', start, end)
+sap_data = web.DataReader('SP500', 'fred', start, end)
 
 start = datetime(1999, 1, 1)
 end = datetime(2019, 1, 1)
 
-nasdaq_data = web.DataReader('NASDAQ100', 'fred', start, end)
-sap_data = web.DataReader('SP500', 'fred', start, end)
 gdp_data = wb.download(indicator='NY.GDP.MKTP.CD', country=['US'], start=start, end=end)
 export_data = wb.download(indicator='NE.EXP.GNFS.CN', country=['US'], start=start, end=end)
-
-def log_return(prices):
-  return np.log(prices / prices.shift(1))
 
 gold_returns = log_return(gold_prices['Gold_Price'])
 crudeoil_returns = log_return(crudeoil_prices['Crude_Oil_Price'])
@@ -27,7 +28,6 @@ export_returns = log_return(export_data['NE.EXP.GNFS.CN'])
 
 print(gold_prices)
 print(crudeoil_prices)
-
 print(nasdaq_data)
 print(sap_data)
 print(gdp_data)
